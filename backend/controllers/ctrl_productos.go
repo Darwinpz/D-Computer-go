f22b0547c8ac2c8e3c733593a7adea40ec2740fe
Darwin Pilaloa Zea <dpilaloa1@utmachala.edu.ec>
2020-02-15 23:22:05 -0500

    obtener productos

--- backend/controllers/ctrl_productos.go
+++ backend/controllers/ctrl_productos.go
@@ -4,13 +4,36 @@ import (
 
 	"encoding/json"
 	"net/http"
+	models "d-computer/backend/models"
 	
-
 )
 
 func GetProductos(res http.ResponseWriter, req *http.Request){
 
-	json.NewEncoder(res).Encode("{productos}")
+	var productos [] models.productos
+
+	obj_producto := new(models.productos)
+	
+	rows, err := GetDB().Query("SELECT * FROM productos")
+
+	if err != nil {
+		return json.NewEncoder(res).Encode(&models.productos{})
+	}
+	
+	defer rows.Close()
+
+	for rows.Next() {
+
+		rows.Scan(
+			&obj_producto.prod_cod,
+			&obj_producto.prod_nombre,
+			&obj_producto.cat_cod
+		)
+
+		productos = append(productos, obj_producto)
+	}
+
+	json.NewEncoder(res).Encode(productos)
 
 }
 
